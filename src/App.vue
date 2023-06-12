462e1eb8c80df60c7b307a41a34e12975c469a3b
leonardordelgado <leonardoresende311015@gmail.com>
2023-06-12 15:40:40 -0300

    manutenção modulo header links

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   
-    <header class="header"><Header /></header>
+    <header class="header"><Header /> </header>
     <main class="main">
       <NavBar />
       <RouterView>
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,19 @@ import Vue ,  { createApp } from 'vue'
 import App from './App.vue'
 import roteador from './router'
 import { store, key} from './store'
+import  VueGtag  from 'vue-gtag-next'
 
 
-import VueAnalytics from 'vue-analytics'
+//"G-JN9LM1KPMV"
 
 
-App.use( VueAnalytics,{
-    id: "G-JN9LM1KPMV"
-})
 
 createApp(App)
+    .use(VueGtag,{
+        property:{
+            id: "G-JN9LM1KPMV"
+        }
+    } )
     .use(roteador)
     .use(store,key)
     .mount('#app')
